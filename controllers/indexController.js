bc682b05c0f5ca899741cd9f75faf5cf2272ebb9
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-28 15:39:46 -0700

    Successfully validate user input

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,65 +4,73 @@ const { body, validationResult } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters.";
+const invalidEmail = "Email must be valid.";
+
+const passwordNoMatch = "Passwords must match";
 
 const validateUser = [
-  body("first_name")
+  body("firstName")
     .trim()
     .isAlpha()
     .withMessage(`First name ${alphaErr}`)
     .isLength({ min: 1, max: 10 })
     .withMessage(`First name ${lengthErr}`),
-    body("last_name").trim()
-    .isAlpha().withMessage(`Last name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+  body("email").isEmail().withMessage(invalidEmail),
+
+
+// Next up we'll need to make sure passwords match, then perform password encryption
+
 
 ];
 
-
-// Next we need to create a view that will render any errors that we get. 
-
-
-
-
 exports.getIndex = async (req, res) => {
   console.log(`getIndex controller function called`);
-
   const users = await userQueries.fetchUsers();
   console.log(`fetched users from query function`);
   console.log(`users.length: ${users.length}`);
   users.forEach((user) => {
     console.log(`user.first_name: ${user.first_name}`);
   });
-
   res.render("./views/pages/home", {
     title: "Home",
     users,
   });
 };
 
-exports.postNewUser = [validateUser, async (req, res) => {
-  console.log(`postNewUser controller function called`);
-
-  console.log(`req.body.firstName: ${req.body.firstName}`);
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).render("./views/pages/home", {
+exports.postNewUser = [
+  validateUser,
+  async (req, res) => {
+    console.log(`postNewUser controller function called`);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(`validation error`);
+      const submission = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+      };
+      return res.status(400).render("./views/pages/home", {
         title: "Home",
-        errors: errors.array()
-    })
-  }
-
-    await userQueries.createUser(
-    req.body.firstName,
-    req.body.lastName,
-    req.body.email,
-    req.body.password,
-    true,
-    false
-  );
-  // Sooooo is it time to validate and sanitize?? probably...
-
-  res.redirect("/");
-}];
+        submission,
+        errors: errors.array(),
+      });
+    } else {
+      console.log(`Submission passed validation`);
+      await userQueries.createUser(
+        req.body.firstName,
+        req.body.lastName,
+        req.body.email,
+        req.body.password,
+        true,
+        false
+      );
+      res.redirect("/");
+    }
+  },
+];
