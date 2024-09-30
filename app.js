cfb17e7460d6eb7a9697712de06e1f84303bc3df
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-30 11:56:51 -0700

    Simplify troubleshooting comments

--- app.js
+++ app.js
@@ -6,33 +6,30 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const indexRouter = require("./routers/indexRouter.js");
+// const indexRouter = require("./routers/indexRouter.js");
 require("dotenv").config();
 
-
-
+const pool = require("./database/pool.js");
 const app = express();
-const port = process.env.PORT;
 
-app.use(express.urlencoded({ extended: true }));
-// This ^^ is needed to access req.body from the form submission
+// app.use(express.urlencoded({ extended: true }));
+// This ^^ is used to access req.body from the form submission. Set to false later by provided instructions so commented out here
 
 app.use(express.static("public"));
 app.set("views", __dirname);
 app.set("view engine", "pug");
 
-// ******************************************************************************************************************************
-// ******************************************************************************************************************************
-// Passport requirements...
-// Not yet working. 
-
-
-const pool = require("./database/pool.js");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+app.listen(process.env.PORT, () => {
+    console.log(`App is listening on port ${process.env.PORT}.`);
+  });
+
+  // ******************************************************************************************************************************
+// The below function never gets called. Why not??
 passport.use(
   new LocalStrategy(async (email, password, done) => {
     console.log(`local strategy triggered`);
@@ -73,48 +70,37 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-app.post("/logIn", passport.authenticate("local", {
+// // Syntax from the lesson, does not reach the desired passport local strategy function
+// app.post("/logIn",
+//     passport.authenticate("local", {
+//         successRedirect: "/",
+//         failureRedirect: "/",
+//       })
+// );
+
+app.post("/logIn", async (req, res) => {
+  console.log(`app post reached`);
+  console.log(`req.body: ${JSON.stringify(req.body)}`);
+  //  Why isn't the next function getting called? Or, what is going wrong? 
+  passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/",
   })
-);
+//   Unless I redirect it will just keep thinking
+  res.redirect("/");
+});
 
-// app.post("/logIn", async (req, res) => {
-//   console.log(`app post reached`);
-//   console.log(`req.body: ${JSON.stringify(req.body)}`);
-
-//   passport.authenticate("local", {
-//     successRedirect: "/",
-//     failureRedirect: "/",
-//   })
-// //   Okayyy so it is getting email and password....
-// //   res.redirect("/");
-// });
-
-const userQueries = require("./database/queries/userQueries.js");
 
 app.get("/", async (req, res) => {
   console.log(`get App function called`);
-
   console.log(`req.user: ${req.user}`);
-
-  const users = await userQueries.fetchUsers();
   res.render("./views/pages/home", {
     title: "Home",
     user: req.user,
-    users,
   });
 });
 
 // ******************************************************************************************************************************
-
+// Once we get authentication working we can re-organize routing to designated files
 // app.use("/", indexRouter);
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}.`);
-  console.log(`.`);
-  console.log(`.`);
-  console.log(`.`);
-  console.log(`.`);
-  console.log(`MAY THE FORCE BE WITH YOU`);
-});
